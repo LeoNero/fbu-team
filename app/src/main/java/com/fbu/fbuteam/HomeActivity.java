dd2aea58f869b60f6161c73951004cf401647281
LeoNero <leonardofelipenerone@gmail.com>
2019-07-17 12:10:30 -0700

    home: add layout and java files

--- app/src/main/java/com/fbu/fbuteam/HomeActivity.java
+++ app/src/main/java/com/fbu/fbuteam/HomeActivity.java
@@ -1,11 +1,8 @@
 package com.fbu.fbuteam;
 
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,15 +12,5 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
