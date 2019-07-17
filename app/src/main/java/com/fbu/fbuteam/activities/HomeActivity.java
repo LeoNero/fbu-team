1fa9174d447ee8f9fa86000ee11a2ef2b6c99665
LeoNero <leonardofelipenerone@gmail.com>
2019-07-17 12:14:12 -0700

    chore: create activities package

--- app/src/main/java/com/fbu/fbuteam/activities/HomeActivity.java
+++ app/src/main/java/com/fbu/fbuteam/activities/HomeActivity.java
@@ -1,8 +1,10 @@
-package com.fbu.fbuteam;
+package com.fbu.fbuteam.activities;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.fbu.fbuteam.R;
 
 public class HomeActivity extends AppCompatActivity {
 
