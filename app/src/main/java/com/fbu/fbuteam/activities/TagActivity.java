69857117adea28aaec8fbfc75386078b7f5ee5bc
Haley Harelson <hharelso@asu.edu>
2019-07-18 13:52:49 -0700

    activities: added initializeObjects() to TagActivity

--- app/src/main/java/com/fbu/fbuteam/activities/TagActivity.java
+++ app/src/main/java/com/fbu/fbuteam/activities/TagActivity.java
@@ -2,15 +2,47 @@ package com.fbu.fbuteam.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.fbu.fbuteam.R;
 
 public class TagActivity extends AppCompatActivity {
 
+    //declare TextView and Button objects
+    private TextView textView1;
+    private TextView textView2;
+    private Button sportsTag;
+    private Button entTag;
+    private Button moneyTag;
+    private Button techTag;
+    private Button envTag;
+    private Button govTag;
+    private Button socialTag;
+    private Button militaryTag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tag);
+
+        initializeObjects();
+
+    }
+
+    private void initializeObjects() {
+        //initialize textView and Button objects
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+        sportsTag = findViewById(R.id.sportsTag);
+        entTag = findViewById(R.id.entTag);
+        moneyTag = findViewById(R.id.moneyTag);
+        techTag = findViewById(R.id.techTag);
+        envTag = findViewById(R.id.envTag);
+        govTag = findViewById(R.id.govTag);
+        socialTag = findViewById(R.id.socialTag);
+        militaryTag = findViewById(R.id.militaryTag);
     }
 }
