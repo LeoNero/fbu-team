8c797b9f14c34f6ebdb820419735462308223a05
Haley Harelson <hharelso@asu.edu>
2019-07-18 14:32:12 -0700

    activities: added logout button listener in HomeActivity layout: created Toolbar in HomeActivity menu resource file: created menu_main.xml and added logout button icon

--- app/src/main/java/com/fbu/fbuteam/activities/HomeActivity.java
+++ app/src/main/java/com/fbu/fbuteam/activities/HomeActivity.java
@@ -1,10 +1,15 @@
 package com.fbu.fbuteam.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.fbu.fbuteam.R;
+import com.parse.ParseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,5 +19,29 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        setLogoutBtnListener(toolbar);
+    }
+
+    private void setLogoutBtnListener(Toolbar toolbar) {
+        toolbar.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.miLogout:
+                    ParseUser.logOut();
+                    final Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+            }
+            return true;
+        });
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
