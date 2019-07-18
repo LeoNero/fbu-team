f2ca999baffff03e6ca80f3ec3060c3d7b03ce72
Haley Harelson <hharelso@asu.edu>
2019-07-18 13:42:46 -0700

    activities: created TagActivity class and skeleton UI for activity_tag.xml

---
+++ app/src/main/java/com/fbu/fbuteam/activities/TagActivity.java
@@ -0,0 +1,16 @@
+package com.fbu.fbuteam.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fbu.fbuteam.R;
+
+public class TagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tag);
+    }
+}
