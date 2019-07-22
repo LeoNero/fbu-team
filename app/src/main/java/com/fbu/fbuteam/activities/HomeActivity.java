4d644549c9ddecd03bcad315ae472d061b226a6e
LeoNero <leonardofelipenerone@gmail.com>
2019-07-22 15:21:02 -0700

    activity: add News and Profile fragments to HomeActivity

--- app/src/main/java/com/fbu/fbuteam/activities/HomeActivity.java
+++ app/src/main/java/com/fbu/fbuteam/activities/HomeActivity.java
@@ -7,17 +7,43 @@ import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.fbu.fbuteam.R;
 import com.parse.ParseUser;
+import com.fbu.fbuteam.fragments.NewsFragment;
+import com.fbu.fbuteam.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class HomeActivity extends AppCompatActivity {
+public class  HomeActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private BottomNavigationView bnvHome;
+
+    private FragmentManager fragmentManager;
+    private Fragment newsFragment;
+    private Fragment profileFragment;
+
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setupComponents();
+        setupToolbar();
+        setupFragments();
+        setupBottomNavigationViewItemSelectedListener();
+        setupDefaultFragment();
+    }
+
+    private void setupComponents() {
+        toolbar = findViewById(R.id.toolbar);
+        bnvHome = findViewById(R.id.bottomNavigationView);
+    }
+
+    private void setupToolbar() {
         setSupportActionBar(toolbar);
 
         setLogoutBtnListener(toolbar);
@@ -45,5 +71,37 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    private void setupFragments() {
+        fragmentManager = getSupportFragmentManager();
+
+        newsFragment = new NewsFragment();
+        profileFragment = new ProfileFragment();
+    }
+
+    private void setupBottomNavigationViewItemSelectedListener() {
+        bnvHome.setOnNavigationItemSelectedListener((item) -> {
+            switch (item.getItemId()) {
+                case R.id.action_news:
+                    currentFragment = newsFragment;
+                    break;
+                case R.id.action_profile:
+                    currentFragment = profileFragment;
+                    break;
+            }
+
+            openSelectedFragment();
+            return true;
+        });
+    }
+
+    private void setupDefaultFragment() {
+        bnvHome.setSelectedItemId(R.id.action_news);
+        currentFragment = newsFragment;
+    }
+
+    private void openSelectedFragment() {
+        fragmentManager.beginTransaction().replace(R.id.frameLayoutHome, currentFragment).commit();
     }
 }
