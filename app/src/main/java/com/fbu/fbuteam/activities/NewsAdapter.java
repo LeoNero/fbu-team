b93a477f93fcb204d1a12b26017eda2effa1c82a
Plaxides Njeri <plaxidesnjeri@fb.com>
2019-07-23 13:25:32 -0700

    OnCreate/OnBind ViewHolders + getItemCount methods

--- app/src/main/java/com/fbu/fbuteam/activities/NewsAdapter.java
+++ app/src/main/java/com/fbu/fbuteam/activities/NewsAdapter.java
@@ -14,14 +14,43 @@ import com.bumptech.glide.Glide;
 import com.fbu.fbuteam.R;
 import com.parse.ParseFile;
 
-public class NewsAdapter {
+import org.w3c.dom.Node;
+
+import java.util.List;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Node> news;
+
+    public NewsAdapter(Context context, List<Node> news) {
+        this.context = context;
+        this.news = news;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_news, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Node news = news.get(position);
+        holder.bind(news);
+    }
+
+    @Override
+    public int getItemCount() {
+        return news.size();
+    }
 
     class ViewHolder extends RecyclerView.ViewHolder {
 
         private TextView tvTitle;
         private TextView tvCreatedAt;
         private TextView tvBodySnippet;
-
 
 
         public ViewHolder(View itemView) {
@@ -32,7 +61,10 @@ public class NewsAdapter {
 
         }
 
+    }
 
+    public void bind(Node news) {
+        //TODO: bind the view elements to the post
     }
 
 }
