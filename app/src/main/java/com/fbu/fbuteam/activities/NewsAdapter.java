1bb42185a6758c582077c83587151062fcfa764c
Plaxides Njeri <plaxidesnjeri@fb.com>
2019-07-23 13:19:29 -0700

    ViewHolder method

--- app/src/main/java/com/fbu/fbuteam/activities/NewsAdapter.java
+++ app/src/main/java/com/fbu/fbuteam/activities/NewsAdapter.java
@@ -1,4 +1,39 @@
 package com.fbu.fbuteam.activities;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.fbu.fbuteam.R;
+import com.parse.ParseFile;
+
 public class NewsAdapter {
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvTitle;
+        private TextView tvCreatedAt;
+        private TextView tvBodySnippet;
+
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvCreatedAt = itemView.findViewById(R.id.tvCreatedAt);
+            tvBodySnippet = itemView.findViewById(R.id.tvBodySnippet);
+
+        }
+
+
+    }
+
 }
+
