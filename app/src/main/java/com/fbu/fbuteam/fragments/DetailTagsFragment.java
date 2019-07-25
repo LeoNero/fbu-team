7504dbb57cc9b141d68b480d442758325f89b17f
Haley Harelson <hharelso@asu.edu>
2019-07-25 10:32:23 -0700

    activities: added ft.addToBackStack("backstackName"); in TagActivity

--- app/src/main/java/com/fbu/fbuteam/fragments/DetailTagsFragment.java
+++ app/src/main/java/com/fbu/fbuteam/fragments/DetailTagsFragment.java
@@ -1,20 +1,14 @@
 package com.fbu.fbuteam.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import com.fbu.fbuteam.R;
-import com.fbu.fbuteam.activities.HomeActivity;
-import com.fbu.fbuteam.activities.TagActivity;
-
 import java.util.ArrayList;
 import java.util.List;
 
