d8efb13a06d7c236bcff41659945fe76eb5a2dff
Plaxides Njeri <plaxidesnjeri@fb.com>
2019-07-29 15:40:56 -0700

    Preferences xml file. Layout for preferences screen

--- app/src/main/java/com/fbu/fbuteam/fragments/SettingsFragment.java
+++ app/src/main/java/com/fbu/fbuteam/fragments/SettingsFragment.java
@@ -5,17 +5,26 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
 
 import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toolbar;
 
 import com.fbu.fbuteam.R;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
-   
+    //To retrieve instances of preferences saved as key value pairs
+    //SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(android.content.Context);
 
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
 }
