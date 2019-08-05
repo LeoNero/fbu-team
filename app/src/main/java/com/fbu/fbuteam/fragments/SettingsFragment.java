bcc3e0a440af6a80729ec1f94fe6f5cad6a76744
Plaxides Njeri <plaxidesnjeri@fb.com>
2019-08-05 15:42:16 -0700

    Settings non-mockups with intents + parse code

--- app/src/main/java/com/fbu/fbuteam/fragments/SettingsFragment.java
+++ app/src/main/java/com/fbu/fbuteam/fragments/SettingsFragment.java
@@ -1,6 +1,6 @@
-
 package com.fbu.fbuteam.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -8,6 +8,11 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.fbu.fbuteam.R;
+import com.fbu.fbuteam.activities.LoginActivity;
+import com.parse.DeleteCallback;
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
@@ -18,7 +23,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         Preference cache = (Preference) findPreference(getString(R.string.clear_cache));
         cache.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             public boolean onPreferenceClick(Preference preference) {
-                Toast.makeText(requireContext(), "Mockup!", Toast.LENGTH_LONG).show();
+                Toast.makeText(requireContext(), "Test!", Toast.LENGTH_LONG).show();
                 return true;
             }
         });
@@ -26,7 +31,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         Preference logout = (Preference) findPreference(getString(R.string.logout));
         logout.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             public boolean onPreferenceClick(Preference preference) {
-                //do something
+                logout();
                 return true;
             }
         });
@@ -34,11 +39,53 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         Preference deleteAccount = (Preference) findPreference(getString(R.string.delete_account));
         deleteAccount.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             public boolean onPreferenceClick(Preference preference) {
-                //do something
+                deleteAcc();
+                logout();
                 return true;
             }
 
         });
 
+    }
+
+    private void logout() {
+        ParseUser user = ParseUser.getCurrentUser();
+        user.logOutInBackground(new LogOutCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    if (user == null) {
+                        goToLogin();
+                    }
+                } else {
+                    Toast.makeText(getContext(), "Something went wrong. Please try again.", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+
+                }
+            }
+        });
+    }
+
+    private void deleteAcc() {
+        ParseUser user = ParseUser.getCurrentUser();
+        user.deleteInBackground(new DeleteCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Toast.makeText(getContext(), "Account deleted!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                } else {
+                    Toast.makeText(getContext(), "Something went wrong. Please try again.", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+
+        });
+    }
+
+    private void goToLogin(){
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
+        getActivity().finish();
     }
 }
