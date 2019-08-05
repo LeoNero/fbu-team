d75a3f065e0c63132a34ea9d0b59fa326fed9317
LeoNero <leonardofelipenerone@gmail.com>
2019-08-05 14:51:50 -0700

    activities/login: add temporary redirect to Profile after login

--- app/src/main/java/com/fbu/fbuteam/activities/LoginActivity.java
+++ app/src/main/java/com/fbu/fbuteam/activities/LoginActivity.java
@@ -91,7 +91,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void goToHome() {
-        Intent intent = new Intent(this, HomeActivity.class);
+        Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
         finish();
     }
