33a8c54672c8231c10f712b7292b28b61a07cecb
Plaxides Njeri <plaxidesnjeri@fb.com>
2019-08-05 16:54:18 -0700

    News article details resource file w/ news article attributes

---
+++ app/src/main/java/com/fbu/fbuteam/fragments/NewsArticleDetailsFragment.java
@@ -0,0 +1,21 @@
+package com.fbu.fbuteam.fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.fbu.fbuteam.Models.NewsArticle;
+import com.fbu.fbuteam.R;
+
+public class NewsArticleDetailsFragment extends Fragment {
+
+
+}
+
