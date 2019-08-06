d395e6043247b0ca238c91d197ef36a32a87caf7
Haley Harelson <hharelso@asu.edu>
2019-08-06 00:32:11 -0700

    adapters: created TopicsAdapter for displaying user selected tags in TopicsFragment fragments: moved logic from TopicsFragment into TopicsAdapter

---
+++ app/src/main/java/com/fbu/fbuteam/adapters/TopicsAdapter.java
@@ -0,0 +1,74 @@
+package com.fbu.fbuteam.adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.fbu.fbuteam.R;
+import com.fbu.fbuteam.models.Node;
+
+import java.util.List;
+
+public class TopicsAdapter extends RecyclerView.Adapter<TopicsAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Node> userTags;
+
+    public TopicsAdapter(Context context, List<Node> userTags) {
+        this.context = context;
+        this.userTags = userTags;
+    }
+
+    @NonNull
+    @Override
+    public TopicsAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_taglist, parent, false);
+        return new TopicsAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TopicsAdapter.ViewHolder holder, int position) {
+        Node userTag = userTags.get(position);
+        displayTags(userTags, holder);
+    }
+
+    private void displayTags(List<Node> userTags, TopicsAdapter.ViewHolder holder) {
+        for (Node tag : userTags) {
+            tag.fetchIfNeededInBackground((res, e) -> {
+                String name = ((Node) res).getName();
+                int level = ((Node) res).getLevel();
+                holder.topicView.setText(name);
+                if (level == 1) {
+                    holder.topicView.setTextColor(Color.RED); //test colors
+                } else {
+                    holder.topicView.setTextColor(Color.GREEN);
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return userTags.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        CardView cardView;
+        TextView topicView;
+
+        private ViewHolder(View itemView) {
+            super(itemView);
+
+            cardView = itemView.findViewById(R.id.cvTopic);
+            topicView = itemView.findViewById(R.id.topicText);
+        }
+    }
+}
