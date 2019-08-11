46487d75e3f3b76fb58da440deb35d912553f36b
Plaxides Njeri <plaxidesnjeri@fb.com>
2019-08-11 16:14:12 -0700

    Creating tool bar and bottom nav in home activity

--- app/src/main/java/com/fbu/fbuteam/activities/HomeActivity.java
+++ app/src/main/java/com/fbu/fbuteam/activities/HomeActivity.java
@@ -1,44 +1,55 @@
 package com.fbu.fbuteam.activities;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.fbu.fbuteam.R;
-import com.parse.ParseUser;
+import com.fbu.fbuteam.fragments.NewsFragment;
+import com.fbu.fbuteam.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private BottomNavigationView bnvHome;
+
+    private FragmentManager fragmentManager;
+    private Fragment newsFragment;
+    private Fragment profileFragment;
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        setupComponents();
+        setupToolbar();
+        setupFragments();
+        setupBottomNavigationViewItemSelectedListener();
+        setupDefaultFragment();
+
+    }
+
+    private void setupComponents() {
+        toolbar = findViewById(R.id.toolbar);
+        bnvHome = findViewById(R.id.bottomNavigationView);
+    }
+
+    private void setupToolbar() {
         setSupportActionBar(toolbar);
-
-        setLogoutBtnListener(toolbar);
+        toolbar.setBackgroundColor(Color.rgb(121,14,139));
     }
 
-    private void setLogoutBtnListener(Toolbar toolbar) {
-        toolbar.setOnMenuItemClickListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.miLogout:
-                    ParseUser.logOut();
-                    goToLogin();
-                    break;
-            }
-            return true;
-        });
-    }
-
-    private void goToLogin() {
-        final Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+    private void setupFragments() {
+        fragmentManager = getSupportFragmentManager();
+        newsFragment = new NewsFragment();
+        profileFragment = new ProfileFragment();
     }
 
     @Override
@@ -46,4 +57,29 @@ public class HomeActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    private void setupBottomNavigationViewItemSelectedListener() {
+        bnvHome.setOnNavigationItemSelectedListener((item) -> {
+            switch (item.getItemId()) {
+                case R.id.action_news:
+                    currentFragment = newsFragment;
+                    break;
+                case R.id.action_profile:
+                    currentFragment = profileFragment;
+                    break;
+            }
+            openSelectedFragment();
+            return true;
+        });
+    }
+
+    private void setupDefaultFragment() {
+        bnvHome.setSelectedItemId(R.id.action_news);
+        currentFragment = profileFragment;
+    }
+
+    private void openSelectedFragment() {
+        fragmentManager.beginTransaction().replace(R.id.frameLayoutHome, currentFragment).commit();
+    }
+
 }
---
+++ app/src/main/java/com/fbu/fbuteam/fragments/NewsFragment.java
@@ -0,0 +1,19 @@
+package com.fbu.fbuteam.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.fbu.fbuteam.R;
+
+public class NewsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_news2, container, false);
+    }
+}
