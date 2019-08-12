ee0f59ae55c546bb1e5f2cde41b8b58c4cefecaf
Plaxides Njeri <plaxidesnjeri@fb.com>
2019-08-11 17:38:01 -0700

    Profile fragment on click of bnv

--- app/src/main/java/com/fbu/fbuteam/activities/ProfileActivity.java
+++ app/src/main/java/com/fbu/fbuteam/activities/ProfileActivity.java
@@ -33,7 +33,7 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.fragment_profile3);
 
         setupComponents();
         setupViewPagerAdapter();
